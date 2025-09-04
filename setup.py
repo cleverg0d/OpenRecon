17416e6b417afd6a80114a6cb2e7c5e29e977573
cleverg0d <sistyagin@gmail.com>
2025-09-04 23:38:13 +0500

    Fix all dependencies and packaging for pipx

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
 setup(
     name='openrecon',
     version='1.0.0',
     author='Stanislav Istyagin (@clevergod)',
     description='🛰️ Asynchronous Reconnaissance Tool for Domain Enumeration and Subdomain Discovery',
     packages=find_packages(),
-    install_requires=[i.strip() for i in open("requirements.txt").readlines()],
+    install_requires=required,
     entry_points={
         'console_scripts': [
             'openrecon=openrecon.openrecon:main',
